5eb9b0ba0b9f5944195c41197c3d232fbfaafba4
Henrique Puppi <kikepuppi@gmail.com>
2023-05-22 09:55:00 -0300

    nada importante

--- Outros/testeaaaaaaaaaa.py
+++ Outros/testeaaaaaaaaaa.py
@@ -1,7 +1,45 @@
-#x = 'sem ovnis no ceu'
 
-#print(x[::-1][2:10])
+# pygame setup
+import pygame as pg
 
-vs = {'secao': 102,'area': 'litoral','candidatos': {'ze da esquina': {'penis': 99},'maria do milagre': 231,'nulos': 43,'brancos': 43}},{'secao': 103,'area': 'litoral','candidatos': {'ze da esquina': 432,'maria do milagre': 965,'nulos': 63,'brancos': 86}},{'secao': 431,'area': 'capital','candidatos': {'tiao da borracharia': 723,'maria do milagre': 812,'nulos': 3,'brancos': 36}}
-print(vs[0['candidatos'['ze da esquina'['penis']]]])
-print(vs[0])
+pg.init()
+screen = pg.display.set_mode([600, 400])
+pg.display.set_caption("Example code for the cursors module")
+
+# create a system cursor
+system = pg.cursors.Cursor(pg.SYSTEM_CURSOR_NO)
+
+# create bitmap cursors
+bitmap_1 = pg.cursors.Cursor(*pg.cursors.arrow)
+bitmap_2 = pg.cursors.Cursor(
+    (24, 24), (0, 0), *pg.cursors.compile(pg.cursors.thickarrow_strings)
+)
+
+# create a color cursor
+surf = pg.Surface((40, 40)) # you could also load an image 
+surf.fill((120, 50, 50))        # and use that as your surface
+color = pg.cursors.Cursor((20, 20), surf)
+
+cursors = [system, bitmap_1, bitmap_2, color]
+cursor_index = 0
+
+pg.mouse.set_cursor(cursors[cursor_index])
+
+clock = pg.time.Clock()
+going = True
+while going:
+    clock.tick(60)
+    screen.fill((0, 75, 30))
+    pg.display.flip()
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+            going = False
+
+        # if the mouse is clicked it will switch to a new cursor
+        if event.type == pg.MOUSEBUTTONDOWN:
+            cursor_index += 1
+            cursor_index %= len(cursors)
+            pg.mouse.set_cursor(cursors[cursor_index])
+
+pg.quit()
